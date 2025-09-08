3ea14674a95c5c8eea99c395c9b7f071a1a38cef
AndresMatamalaCaamano <amatamalac@it.ucsc.cl>
2025-09-08 10:51:14 -0300

    Delete java.js

--- java.js
+++
@@ -1,6 +0,0 @@
-let rotacion = 0;
-const contenedor = document.getElementById("contenedor");
-contenedor.addEventListener("click", function() {
-    rotacion += 90;
-    this.style.transform = `rotate(${rotacion}deg)`;
-});
